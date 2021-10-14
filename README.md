7cf25814fa3f1bb61b03347b9b9eb21e99874417
Evgeny Taruntaev <eugene@cinegy.com>
2021-10-14 15:37:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# FFmpeg-limited-vcpkg: Overview
+
+This repository contains build script and patch files to automate build of FFmpeg library in the way how it used in the Cinegy software.
+
+# Prerequisites
+
+You should install Git command line client and Visual Studio 2019.
+
+# Instructions
+
+Clone repository and run ```ffmpeg-limited-vcpkg.bat```.
---
+++ ffmpeg-limited-vcpkg.bat
@@ -0,0 +1,35 @@
+@echo off
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+cd .\vcpkg
+git checkout d679a1e0befa9c9e36fcda169c94f6daba2224b7
+if ERRORLEVEL 1 goto Error
+git apply ..\ffmpeg-limited.diff
+if ERRORLEVEL 1 goto Error
+
+call bootstrap-vcpkg.bat -disableMetrics
+if ERRORLEVEL 1 goto Error
+
+vcpkg install ffmpeg[core,swresample,swscale,avfilter,avcodec]:x86-windows --recurse
+if ERRORLEVEL 1 goto Error
+vcpkg install ffmpeg[core,swresample,swscale,avfilter,avcodec]:x64-windows --recurse
+if ERRORLEVEL 1 goto Error
+
+@cd /d "%~dp0"
+
+@REM set VER=4.4#14-limited-1
+@REM set EXCLUDE=-x!share -x!tools -xr!ffnvcodec* -xr!pkgconfig
+
+@REM %SEVENZIP% a ffmpeg-%VER%-x86.7z .\vcpkg\installed\x86-windows\* %EXCLUDE%
+@REM if ERRORLEVEL 1 goto Error
+@REM %SEVENZIP% a ffmpeg-%VER%-x64.7z .\vcpkg\installed\x64-windows\* %EXCLUDE%
+@REM if ERRORLEVEL 1 goto Error
+
+exit /b 0
+
+:Error
+pause
+@cd /d "%~dp0"
+if exist .\vcpkg rmdir /s /q .\vcpkg
+exit /b 1
