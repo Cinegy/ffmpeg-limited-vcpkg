ae0308954cefe2c63dbcd5c1ce80c3f90d42a2ba
Evgeny Taruntaev <eugene@cinegy.com>
2021-10-14 19:31:01 +0200

    Improved 7-Zip detection procedure.

--- ffmpeg-limited-vcpkg.bat
+++ ffmpeg-limited-vcpkg.bat
@@ -6,8 +6,8 @@ set VERSUFFIX=-limited-1
 
 set VCPKG=.\vcpkg
 
-if exist %VCPKG% rmdir /s /q %VCPKG%
 del /q ffmpeg-*.7z >nul
+if exist %VCPKG% rmdir /s /q %VCPKG%
 
 git clone --no-checkout https://github.com/microsoft/vcpkg
 if ERRORLEVEL 1 goto FAILED
@@ -30,28 +30,21 @@ cd /d "%~dp0"
 
 for /f "tokens=2" %%I in ('%VCPKG%\vcpkg list ffmpeg:x64') do set VER=%%I%VERSUFFIX%
 
-where 7z >nul
+for /d %%I in (%VCPKG%\downloads\tools\7zip*) do set LOCALPATH=;%%I\Files\7-Zip
+set PATH=%PATH%;%ProgramFiles%\7-Zip;%ProgramFiles(x86)%\7-Zip%LOCALPATH%
+
+where 7z 2>&1 1>nul
 
 if ERRORLEVEL 1 (
-    if exist "C:\Program Files\7-Zip\7z.exe" (
-      set SEVENZIP="C:\Program Files\7-Zip\7z.exe"
-    ) else (
-      for /D %%I in (%VCPKG%\downloads\tools\7zip*) do set SEVENZIP="%%I\Files\7-Zip\7z.exe"
-    )
-) else (
-    set SEVENZIP=7z
-)
-
-if [%SEVENZIP%]==[] (
-  echo "7Zip x64 not installed - please install and retry"
+  echo "7-Zip not installed - please install and retry"
   goto FAILED
 )
 
 set EXCLUDE=-x!share -x!tools -xr!ffnvcodec* -xr!pkgconfig
 
-%SEVENZIP% a ffmpeg-%VER%-x86.7z %VCPKG%\installed\x86-windows\* %EXCLUDE%
+7z a ffmpeg-%VER%-x86.7z %VCPKG%\installed\x86-windows\* %EXCLUDE%
 if ERRORLEVEL 1 goto FAILED
-%SEVENZIP% a ffmpeg-%VER%-x64.7z %VCPKG%\installed\x64-windows\* %EXCLUDE%
+7z a ffmpeg-%VER%-x64.7z %VCPKG%\installed\x64-windows\* %EXCLUDE%
 if ERRORLEVEL 1 goto FAILED
 
 exit /b 0
