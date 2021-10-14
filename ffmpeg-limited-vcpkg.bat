44c22e58c5287f1e17a8697f58aff1f1f9420f73
Evgeny Taruntaev <eugene@cinegy.com>
2021-10-14 15:53:00 +0200

    Add missed clone command to the sript, clean vcpkg folder before run

--- ffmpeg-limited-vcpkg.bat
+++ ffmpeg-limited-vcpkg.bat
@@ -1,6 +1,11 @@
 @echo off
-@setlocal enableextensions
-@cd /d "%~dp0"
+setlocal enableextensions
+cd /d "%~dp0"
+
+if exist .\vcpkg rmdir /s /q .\vcpkg
+
+git clone --no-checkout https://github.com/microsoft/vcpkg
+if ERRORLEVEL 1 goto error
 
 cd .\vcpkg
 git checkout d679a1e0befa9c9e36fcda169c94f6daba2224b7
@@ -16,7 +21,7 @@ if ERRORLEVEL 1 goto Error
 vcpkg install ffmpeg[core,swresample,swscale,avfilter,avcodec]:x64-windows --recurse
 if ERRORLEVEL 1 goto Error
 
-@cd /d "%~dp0"
+cd /d "%~dp0"
 
 @REM set VER=4.4#14-limited-1
 @REM set EXCLUDE=-x!share -x!tools -xr!ffnvcodec* -xr!pkgconfig
@@ -30,6 +35,5 @@ exit /b 0
 
 :Error
 pause
-@cd /d "%~dp0"
-if exist .\vcpkg rmdir /s /q .\vcpkg
+cd /d "%~dp0"
 exit /b 1
